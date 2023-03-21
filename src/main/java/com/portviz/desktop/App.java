cca6b8dc349d554299dc5d1890080b602d63b8eb
pendyurinandrey <pendyurinandrey@gmail.com>
2023-03-21 20:26:02 +0500

    Initial project structure

---
+++ src/main/java/com/portviz/desktop/App.java
@@ -0,0 +1,22 @@
+package com.portviz.desktop;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
